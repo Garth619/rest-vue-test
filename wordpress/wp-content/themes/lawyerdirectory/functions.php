ca8f05768e8e692f028b720ab1a481f370ccf49e
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-03 12:48:32 -0700

    push

--- wordpress/wp-content/themes/lawyerdirectory/functions.php
+++ wordpress/wp-content/themes/lawyerdirectory/functions.php
@@ -11,3 +11,41 @@ add_theme_support('post-thumbnails');
 
 
 
+/* Lawyer Firm CPT 
+-------------------------------------------------------------- */
+
+
+ 	
+      
+   	 
+	function lawfirm() {    
+    	$args = array(    
+        	'label' => __('Lawfirms'),    
+        	'singular_label' => __('Lawfirm'),    
+        	'public' => true,    
+        	'show_ui' => true,
+        	'has_archive' => true,	 
+        	'capability_type' => 'post',    
+        	'hierarchical' => false,    
+        	'rewrite' => true,
+        	'show_in_rest' => true,    
+        	'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )    
+       	);    
+   	 
+    	register_post_type( 'lawfirm' , $args );    
+	}  
+	
+	
+	add_action('init', 'lawfirm');
+	
+	  
+	register_taxonomy(
+		"lawfirm-cat", 
+		array("lawfirm"),
+		array(
+			"hierarchical" => true,
+			'show_in_rest' => true,
+			"label" => "Lawfirm Cats",
+			"singular_label" => "Lawfirm Cat",
+			"rewrite" => true
+		)); 
