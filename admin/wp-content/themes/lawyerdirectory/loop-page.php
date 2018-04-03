006119b597e4fcb89a93a55f5d3957f268a6c201
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-03 09:28:33 -0700

    push

--- admin/wp-content/themes/lawyerdirectory/loop-page.php
+++
@@ -1,9 +0,0 @@
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-	<?php the_content();?>
-	
-	<?php edit_post_link( __( 'Edit'), '', '' ); ?>
-
-<?php endwhile; // end of loop ?>
-
-<?php endif; ?>
---
+++ wp-content/themes/lawyerdirectory/functions.php
@@ -0,0 +1,56 @@
+<?php 
+
+
+/* Enqueued Scripts
+-------------------------------------------------------------- */
+
+
+
+ function load_my_styles_scripts() {
+     // Load my stylesheet
+     wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', '', 1, 'all' ); 
+
+     // Load my javascripts
+     // wp_enqueue_script( 'jquery-addon', get_template_directory_uri() . '/js/custom-min.js', array('jquery'), '', true );
+ }
+ 
+ add_action( 'wp_enqueue_scripts', 'load_my_styles_scripts', 20 );
+ 
+
+
+
+
+/* Remove Unnecessary Scripts
+-------------------------------------------------------------- */
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
+
+/* Register Nav-Menus
+-------------------------------------------------------------- */
+
+register_nav_menus(array(
+    'main_menu' => 'Main Menu',
+    
+));
+
+
+/* Add Theme Support Page Thumbnails
+-------------------------------------------------------------- */
+
+add_theme_support('post-thumbnails');
+
+/* Modify the_excerpt() " read more "
+-------------------------------------------------------------- */
+
+function new_excerpt_more($more)
+{
+    global $post;
+    return '... <a href="' . get_permalink($post->ID) . '">' . 'read more' . '</a>';
+}
+
+add_filter('excerpt_more', 'new_excerpt_more');
+
+
+
