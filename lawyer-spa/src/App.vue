e1b038b0b3183f905a4830cf922785464a51d093
Garrett Cullen <garrettwcullen@gmail.com>
2018-04-04 09:37:26 -0700

    push

--- lawyer-spa/src/App.vue
+++ lawyer-spa/src/App.vue
@@ -1,23 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header>
+      <span>Handling Ajax Request with Axios in Vue</span>
+    </header>
+    <main>
+        <h2>Click the button to get Random jokes</h2>
+        <button id="btn" class="" v-on:click="getJokes">Get Jokes</button>
+
+        <div v-if="loading">
+          <img src="../src/assets/logo.png"/>
+          Loading.....
+        </div>
+
+      <div class="wrapper">
+        <div class="row">
+          <div v-for="joke in jokes" :key="joke.id">
+          <div class="col-md-4 cards">
+             <img src="https://placeimg.com/300/300/nature" class="img-responsive" alt="Random images placeholder"> 
+            <div>
+              <h3>{{ joke.id }}</h3>
+              <p>{{ joke.joke }}</p>
+              <p>{{ joke.category }}</p>
+            </div>
+          </div>
+        </div>
+        </div>
+        
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
+  //import axios from 'axios';
+  import {API} from './axios'
 export default {
-  name: 'App'
+  name: 'app',
+  data () {
+    return {
+      jokes: [],
+      loading: false
+    }
+  }, 
+  methods: {
+    getJokes: function () {
+      this.loading = true;
+      API.get(`jokes`)
+      .then((response)  =>  {
+        this.loading = false;
+        this.jokes = response.data.value;
+      }, (error)  =>  {
+        console.log(error);
+        this.loading = false;
+      })
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
 </style>
