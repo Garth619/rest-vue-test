794ea1bd2ef3856ca39049106d11af6d10e93183
Garrett Cullen <garrettwcullen@gmail.com>
2018-05-11 14:29:47 -0700

    push

--- lawyer-spa/src/components/searchRoute.vue
+++ lawyer-spa/src/components/searchRoute.vue
@@ -3,17 +3,18 @@
     
     <section id="main_search">
 
+      <div v-if="searching">
+	    	<i>Searching...</i>
+	  </div>
+
       <div class="main_search_wrapper">
 
-        <form action="/action_page.php">
-          <input type="text" placeholder="Search.." name="search">
-          <input type="text" placeholder="Search.." name="search">
-          <button type="submit">Search</button>
-      </form>
-    
-    </div><!-- main_search_wrapper -->
+          <input v-model="term" type="search">
+          <button @click="search">Search</button>
+      
+      </div><!-- main_search_wrapper -->
 
-    <div class="browse_wrapper">
+      <div class="browse_wrapper">
       
       <span>Browse by:</span>
 
@@ -29,7 +30,44 @@
     
     </div><!-- browse_wrapper -->
 
+
+      
+
+
+
     </section><!-- main_search -->
+
+    <section class="search_results_wrapper">
+      
+      <div class="results" v-for="pos in posts" :key="pos.id">
+
+      <h1 v-html="pos.title.rendered"></h1>
+
+      <p v-html="pos.content.rendered"></p>
+
+     <div v-for="field in pos.acf.attorneys" :key="field.id">
+
+       <p>{{field.name}}</p>
+
+      <p>{{field.position}}</p>
+
+     </div>
+
+    
+    <div v-for="cat in pos.lawfirmcat" :key="cat.id">
+
+      {{cat}}
+      
+    </div>
+
+
+  </div><!-- results -->
+
+  <div v-if="noResults">
+		Sorry, but no results were found.
+	</div>
+    
+    </section><!-- search_results_wrapper -->
 
   
   </div><!-- search -->
@@ -43,26 +81,34 @@ export default {
   name: 'SearchRoute',
   data () {
     return {
+      term:[],
       posts: [],
-      errors:[]
+      errors:[],
+      noResults:false,
+      searching:false
     }
   },
-  created() {
-    axios.get('http://lawyers-test.com/wp-json/wp/v2/lawfirm')
-    .then(response => {
-      this.posts = response.data
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
-  }
+	methods:{
+		search:function() {
+      this.searching = true;
+      axios.get('http://lawyers-test.com/wp-json/wp/v2/lawfirm?search=' + this.term + '')
+      .then(response => {
+        this.posts = response.data;
+        this.noResults = this.posts.length === 0;
+        this.searching = false;
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
+	}
 }
 </script>
 
 <style scoped lang="scss">
 
 #main_search {
-  height:100vh;
+  height:500px;
   display: flex;
   flex-direction: column;
   justify-content: center;
